a8a42af5aaf6972ad9fc66aa441e4d8b9025bb45
William <bzfishmaster94@gmail.com>
2024-06-06 19:05:31 -0400

    Made it so it uses local storage and the buttons appear below with the input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
 const apiKey = 'b7806c84d1d3d136fad167665de26042'
 const cityName = '';
 const cityInput = document.querySelector('#search-input')
+const searchButton = document.querySelector('#search-btn')
 
+const leftView = document.querySelector('#left-view')
+
+let tempStorageName = JSON.parse(localStorage.getItem('history')) || {
+    city: []
+}
+
+// Connecting to the ApiUrl //
 function test () {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
-
     fetch(apiUrl)
     .then(function (response) {
         if (!response) {
@@ -18,8 +25,42 @@ function test () {
     }) 
 }
 
+
+// Making a form submit that goes into localStorage.
+function inputSubmit (event) {
+    event.preventDefault();
+
+    const cityData = cityInput.value;
+
+    tempStorageName.city.push(cityData);
+
+    localStorage.setItem('history', JSON.stringify(tempStorageName));
+
+    const createNewButton = document.createElement('button');
+
+    createNewButton.textContent = cityData;
+
+    leftView.appendChild(createNewButton);
+}
+
+function recentButtons () {
+    const storedData = JSON.parse(localStorage.getItem('tempStorageName'));
+
+    if (storedData) {
+        tempStorageObject = storedData;
+    }
+
+    for (let i = 0; i < tempStorageName.city.length; i++) {
+        const createNewButton = document.createElement('button');
+        createNewButton.textContent = tempStorageName.city[i];
+        leftView.appendChild(createNewButton);
+    }
+
+    return;
+}
+
+recentButtons(tempStorageName);
+
 test();
 
-function storeLocalStorage() {
-    localStorage.setItem('')
-}
+searchButton.addEventListener('click', inputSubmit)
